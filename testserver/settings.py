5d15e6427caaedfebb8a27a94c6d7270ff4fb96f
mmonj <mmonj@users.noreply.github.com>
2023-06-14 05:27:44 +0000

    group STATIC* variables together

--- testserver/settings.py
+++ testserver/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static/",)
-STATIC_ROOT = BASE_DIR / "collected/"
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -133,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static/",)
+STATIC_ROOT = BASE_DIR / "collected/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
