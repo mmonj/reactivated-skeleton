939b66d8e63bac143e5dfa918127fea7c14dc867
mmonj <mmonj@users.noreply.github.com>
2023-06-14 07:07:39 +0000

    add testapp to server 'urls.py'

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 
 from . import templates
 
-def django_default(request: HttpRequest) -> HttpResponse:
+def testpage(request: HttpRequest) -> HttpResponse:
     return templates.TestPage(
         name="Kenobi", title="General", greeting="Hello there"
     ).render(request)
