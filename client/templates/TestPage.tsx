d0bddfe04deb4965c876520045f3fff62ca4285d
mmonj <mmonj@users.noreply.github.com>
2023-06-14 05:03:48 +0000

    fix props

--- client/templates/TestPage.tsx
+++ client/templates/TestPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import {templates} from "@reactivated";
 import {Layout} from "@client/components/Layout";
 
-export default (props: templates.PollsIndex) => {
+export default (props: templates.TestPage) => {
     return (
         <Layout title="Test Page" >
             <h1>Test Page Main</h1>
