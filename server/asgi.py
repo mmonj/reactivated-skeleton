6f2d499d0dd20708fee7921954c1b8f20f13ca5c
mmonj <mmonj@users.noreply.github.com>
2023-06-17 21:28:01 +0000

    change server name to 'server'

--- server/asgi.py
+++ server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testserver project.
+ASGI config for this project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testserver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_asgi_application()
