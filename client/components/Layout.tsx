da4358b251054164c0187257d92926c981f18193
mmonj <mmonj@users.noreply.github.com>
2023-06-27 00:27:43 -0400

    add styles

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Context } from "@reactivated";
 import { Helmet } from "react-helmet-async";
 
+import "@client/styles/app.css";
+
 interface Props {
   title: string;
   children: React.ReactNode;
